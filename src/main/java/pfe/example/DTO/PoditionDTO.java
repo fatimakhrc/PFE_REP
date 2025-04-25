a955d24cdbb7291fe91de73d8beff3437044b3aa
fatimakhrc <f.kharich@edu.umi.ac.ma>
2025-04-25 20:46:20 +0100

    another commit

---
+++ src/main/java/pfe/example/DTO/PoditionDTO.java
@@ -0,0 +1,35 @@
+package pfe.example.DTO;
+
+public class PoditionDTO {
+    private Long courrierId;
+    private double latitude;
+    private double longitude;
+
+    
+    public PoditionDTO(Long courrierId, double latitude, double longitude) {
+        this.courrierId = courrierId;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    public Long getCourrierId() {
+        return courrierId;
+    }
+    public void setCourrierId(Long courrierId) {
+        this.courrierId = courrierId;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    
+
+}
